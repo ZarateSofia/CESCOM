e2faaa1dc855cda521058073dbbdf40f5ce98747
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2024-03-18 15:27:55 -0500

    Registro y Enrutamiento

--- CESCOM/src/app/app.routes.ts
+++ CESCOM/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashboardComponent},
+    {path: 'register', component: RegisterComponent},
+    {path:'**', redirectTo:''}
+];
--- CESCOM/src/app/pages/dashboard/dashboard.component.ts
+++ CESCOM/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ RouterLinkActive, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
