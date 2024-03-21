8134780326d61d3edef0eee0071225321bdb9de9
ZarateSofia <118627198+ZarateSofia@users.noreply.github.com>
2024-03-21 13:02:12 -0500

    registrador

---
+++ CESCOM/src/app/registrador/registrador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrador',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrador.component.html',
+  styleUrl: './registrador.component.css'
+})
+export class RegistradorComponent {
+
+}
