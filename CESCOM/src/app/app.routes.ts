1be959848e268ff173a85cf137f08f67e187e4be
ZarateSofia <szarate@espol.edu.ec>
2024-03-28 01:43:24 -0500

    REGISTRADOR

--- CESCOM/src/app/app.routes.ts
+++ CESCOM/src/app/app.routes.ts
@@ -6,6 +6,10 @@ import { FAQComponent } from './cliente/faq/faq.component';
 import { Error404Component } from './error404/error404.component';
 import { OrdenesComponent } from './cliente/ordenes/ordenes.component';
 import { RegistradorComponent } from './registrador/registrador.component';
+import { ClientesComponent } from './registrador/clientes/clientes.component'
+import { InventarioComponent } from './registrador/inventario/inventario.component'
+import { ReparacionesComponent } from './registrador/reparaciones/reparaciones.component'
+import { VentasComponent } from './registrador/ventas/ventas.component'
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
@@ -16,7 +20,12 @@ export const routes: Routes = [
         {path: 'ordenes', component: OrdenesComponent}
         
     ]},
-    {path: 'registrador', component: RegistradorComponent},
+    {path: 'registrador', component: RegistradorComponent, children:[
+        {path: 'clientes', component: ClientesComponent},
+        {path: 'inventario', component: InventarioComponent},
+        {path: 'reparaciones', component: ReparacionesComponent},
+        {path: 'ventas', component: VentasComponent},
+    ]},
 
     {path:'**', redirectTo:'/404'}
 ];
